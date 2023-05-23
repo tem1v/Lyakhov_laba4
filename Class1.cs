548a00848db5206f20ffda9cf22458df407d1463
Artjom <Artjom@DESKTOP-99SF1TN>
2023-05-23 20:24:04 +0300

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lyakhov_lab4
+{
+    internal class Vector
+    {
+        public int[] array;    
+
+        public int size;       
+
+        public int capacity;   
+
+        public Vector(int capacity)
+        {
+            this.array = new int[capacity];
+            this.size = 0;
+            this.capacity = capacity;
+        }
+
+        public void Fill(int start, int end)
+        {
+            for (int i = start; i <= end; i++)
+            {
+                Add(i);
+            }
+        }
+
+        public int GetByIndex(int index)
+        {
+            return array[index];
+        }
+
+        public void SetByIndex(int index, int value)
+        {
+            array[index] = value;
+        }
+
+        public void Add(int element)
+        {
+            if (size == capacity)
+            {
+                int newCapacity = capacity * 2;
+                int[] newArray = new int[newCapacity];
+                for (int i = 0; i < size; i++)
+                {
+                    newArray[i] = array[i];
+                }
+                array = newArray;
+                capacity = newCapacity;
+            }
+            array[size] = element;
+            size++;
+        }
+
+        public void Delete()
+        {
+            size--;
+        }
+
+        public void Insert(int index, int element)
+        {
+            if (size == capacity)
+            {
+                int newCapacity = capacity * 2;
+                int[] newArray = new int[newCapacity];
+                for (int i = 0; i < size; i++)
+                {
+                    newArray[i] = array[i];
+                }
+                array = newArray;
+                capacity = newCapacity;
+            }
+
+            for (int i = size - 1; i >= index; i--)
+            {
+                array[i + 1] = array[i];
+            }
+            array[index] = element;
+            size++;
+        }
+
+        public int[] Max()
+        {
+            int maxIndex = 0;
+            for (int i = 1; i < size; i++)
+            {
+                if (array[i] > array[maxIndex])
+                {
+                    maxIndex = i;
+                }
+            }
+            return new int[] { maxIndex, array[maxIndex] };
+        }
+
+        public int Count()
+        {
+            return size;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lyakhov_lab4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите ёмкость массива:");
+            int capacity = int.Parse(Console.ReadLine());
+            Vector array = new Vector(capacity);
+            array.Fill(1, 5);
+            Console.WriteLine("Массив:");
+            for (int i = 0; i < array.Count(); i++)
+            {
+                Console.Write(array.GetByIndex(i) + " ");
+            }
+            Console.WriteLine();
+
+            array.Add(6);
+            Console.WriteLine("Добавление 6:");
+            for (int i = 0; i < array.Count(); i++)
+            {
+                Console.Write(array.GetByIndex(i) + " ");
+            }
+            Console.WriteLine();
+
+            array.Delete();
+            Console.WriteLine("Удаление последнего элемента:");
+            for (int i = 0; i < array.Count(); i++)
+            {
+                Console.Write(array.GetByIndex(i) + " ");
+            }
+            Console.WriteLine();
+
+            array.Insert(2, 10);
+            Console.WriteLine("Вставка 10 в индекс 2:");
+            for (int i = 0; i < array.Count(); i++)
+            {
+                Console.Write(array.GetByIndex(i) + " ");
+            }
+            Console.WriteLine();
+
+            int[] max = array.Max();
+            Console.WriteLine($"Максимальное число:{ max[1]} с индексом {max[0]}");
+            Console.WriteLine($"Количество элементов в массиве: {array.Count()}");
+            Console.ReadLine();
+        }
+    }
+}
